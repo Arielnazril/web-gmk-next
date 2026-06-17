51b1d1fbdfd0163f1ca83e7862b6f115c3f0ebad
Nazril Afdal Riadi <arielnazril133@gmail.com>
2026-06-17 20:49:46 +0700

    update sitemap.ts

--- sitemap.ts
+++ sitemap.ts
@@ -5,8 +5,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://www.sekolahglobalmajukhatulistiwa.sch.id',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
     },
+    {
+      url: 'https://www.sekolahglobalmajukhatulistiwa.sch.id/tentang-kami',
+      lastModified: new Date(),
+    },
+    // Tambahkan halaman lain di sini jika perlu
   ]
 }
