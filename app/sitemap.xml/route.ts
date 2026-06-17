faa9db0df943e1d0ca84fb37a79fc4110c550823
Nazril Afdal Riadi <arielnazril133@gmail.com>
2026-06-17 20:52:34 +0700

    update sitemap to manual route

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,19 @@
+export async function GET() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.sekolahglobalmajukhatulistiwa.sch.id/</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </url>
+  <url>
+    <loc>https://www.sekolahglobalmajukhatulistiwa.sch.id/tentang-kami</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </url>
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
